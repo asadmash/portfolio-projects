1f19b3ed9fc7d670804bf5811e839f86f0046387
asadmash <asaduzzaman.mashuq@gmail.com>
2024-04-05 11:23:00 +0600

    ecomerce-product-page

--- 30.ecommerce-product-page/WORKFLOW.md
+++ 30.ecommerce-product-page/WORKFLOW.md
@@ -1,25 +1,27 @@
-* [X] ~~*DONE:update product total value accourding to users product quanity*~~ [2024-03-20]
-* [X] ~~*DONE:update product total value accourding to users product quanity*~~ [2024-03-20]
+- [x] ~~_DONE:update product total value accourding to users product quanity_~~ [2024-03-20]
+- [x] ~~_DONE:update product total value accourding to users product quanity_~~ [2024-03-20]
 
+- [x] ~~_DONE:add functionality to the add cart button to update cart elements according to current product quantity and price_~~ [2024-03-21]
 
-* [X] ~~*DONE:add functionality to the add cart button to update cart elements according to current product quantity and price*~~ [2024-03-21]
+- [x] ~~_DONE:when user click user profile icon at the top right corner show a dummy profile information prompt as like cart container_~~ [2024-03-21]
 
-* [X] ~~*DONE:when user click user profile icon at the top right corner show a dummy profile information prompt as like cart container*~~ [2024-03-21]
+- [x] ~~_DONE:add functionality to change the galary image by clicking bottom thumbnail //_~~ [2024-03-22]
 
-* [X] ~~*DONE:add functionality to change the galary image by clicking bottom thumbnail //*~~ [2024-03-22]
+- [x] ~~_DONE:add a overlay for gallery when clicked thumbnai button_~~ [2024-03-23]
 
-* [X] ~~*DONE:add a overlay for gallery when clicked thumbnai button*~~ [2024-03-23]
+- [x] ~~_DONE:Hide the overlay gallery after clicking top close button_~~ [2024-03-24]
 
-* [X] ~~*DONE:Hide the overlay gallery after clicking top close button*~~ [2024-03-24]
+- [x] ~~_DONE:show the overlay gallery when user click any thumbnail_~~ [2024-03-24]
 
-* [X] ~~*DONE:show the overlay gallery when user click any thumbnail*~~ [2024-03-24]
+- [x] ~~_DONE:Navigate though the overlay thumbnail and prev next button as like as main gallery_~~ [2024-03-28]
+  - [x] ~~_DONE: navigate though ov by thumb gallery also navigate original gallery_~~ [2024-03-28]
+- [x] ~~_DONE: main gallery any thum image click not navigate to same image of ov gallery_~~ [2024-03-28]
 
-* [X] ~~*DONE:Navigate though the overlay thumbnail and prev next button as like as main gallery*~~ [2024-03-28]
-    * [X] ~~*DONE: navigate though ov by thumb gallery also navigate original gallery*~~ [2024-03-28]
-* [X] ~~*DONE: main gallery any thum image click not navigate to same image of ov gallery*~~ [2024-03-28]
+- [x] ~~_DONE: add another confirmation message page over main page after clicking cart checkout_~~ [2024-04-02]
 
-* [ ] FIXME: after navigating ov gallery using thumbs when click ov prev next button the gallery shows wrong index image. 
-    
- * [ ] TODO: add another confirmation message page over main page after clicking cart checkout
+- [ ] TODO: mobile navigation menu open close functionality
 
-* [ ] TODO: Add feature for delete cart item
+- [ ] TODO: Add feature for delete cart item
+
+- [ ] FIXME: after navigating ov gallery using thumbs when click ov prev next button the gallery shows wrong index image.
+- [ ] FIXME: Cart item count one more then original order quantity
--- 30.ecommerce-product-page/app/app.js
+++ 30.ecommerce-product-page/app/app.js
@@ -1,120 +1,133 @@
 // NOTE: DOM REFERENCE AREA
 
 // NOTE: implmenting slide gallary feature
-const slides = document.querySelectorAll('.slide')
+const slides = document.querySelectorAll(".slide");
 // STEP: OVERLAY SLIDES
-const ovSlides = document.querySelectorAll('.ov-slide')
+const ovSlides = document.querySelectorAll(".ov-slide");
 // NOTE: grab those prev and next btn to change image
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 
 // STEP:OVERLAY PREV NEXT BTN
-const ovPrevBtn = document.querySelector('.ov-prev-btn');
-const ovNextBtn = document.querySelector('.ov-next-btn');
+const ovPrevBtn = document.querySelector(".ov-prev-btn");
+const ovNextBtn = document.querySelector(".ov-next-btn");
 
 // Thumb reference
-const thums = document.querySelectorAll('.thum');
-const thum1 = document.querySelector('.thum1');
-const thum2 = document.querySelector('.thum2');
-const thum3 = document.querySelector('.thum3');
-const thum4 = document.querySelector('.thum4');
+const thums = document.querySelectorAll(".thum");
+const thum1 = document.querySelector(".thum1");
+const thum2 = document.querySelector(".thum2");
+const thum3 = document.querySelector(".thum3");
+const thum4 = document.querySelector(".thum4");
 
-const ovThums = document.querySelectorAll('.ov-thum');
+const ovThums = document.querySelectorAll(".ov-thum");
 // console.log("🚀 ~ ovThums:", ovThums)
 
 // image ref
-const allImage = document.querySelectorAll('.thumb-img');
+const allImage = document.querySelectorAll(".thumb-img");
 
 // cartbox ref
-const cartCont = document.querySelector('.cart-container');
-const cartBtn = document.querySelector('#cart');
+const cartCont = document.querySelector(".cart-container");
+const cartBtn = document.querySelector("#cart");
 
 // Cart ref to update value according to users prduct price and quantity
-const cartItemCount = document.querySelector('.cart-item-count');
-const cartItemPrice = document.querySelector('.cart-item-price');
+const cartItemCount = document.querySelector(".cart-item-count");
+const cartItemPrice = document.querySelector(".cart-item-price");
 const addCartBtn = document.querySelector(".add-cart-btn");
 
 // order quantity ref
-const decreseBtn = document.querySelector('.decrease');
-const increseBtn = document.querySelector('.increase')
-const displayCount = document.querySelector('.count');
+const decreseBtn = document.querySelector(".decrease");
+const increseBtn = document.querySelector(".increase");
+const displayCount = document.querySelector(".count");
 
-const discountPrice = document.querySelector('.discount-price');
+const discountPrice = document.querySelector(".discount-price");
 // console.log("🚀 ~ discountPrice:", discountPrice);
 
 // profile ref
-const profile = document.querySelector('#profile');
-const profCont = document.querySelector('#user-profile');
+const profile = document.querySelector("#profile");
+const profCont = document.querySelector("#user-profile");
 
 // STEP: overlay gallery hide ref
-const ovCloseBtn = document.querySelector('.close-btn');
-const ovGallery = document.querySelector('#ov-gallary');
+const ovCloseBtn = document.querySelector(".ov-close-btn");
+console.log("🚀 ~ ovCloseBtn:", ovCloseBtn);
+const ovGallery = document.querySelector("#ov-gallary");
+
+// STEP: CHECKBUTTON REF
+const checkBtn = document.querySelector(".checkout-btn");
+const checkConfirm = document.querySelector(".checkout-confirmation");
+const greetCloseBtn = document.querySelector(".greetings-close-btn");
+
+// STEP: MOBILE MENU REF
+const mobileMenu = document.querySelector(".mobile-menu");
+const navOpenBtn = document.querySelector("#mobile-toggle");
+const navCloseBtn = document.querySelector(".menu-close");
 
 // STEP: SHOW OVERLAY GALLERY
 // thums.addEventListener('click', showOverlay);
 
-
-
 // NOTE: EVENT lISTENER AREA
 
 // Add event listener to the slide button
-prevBtn.addEventListener('click', prevSlide);
-nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
 // STEP: ADD EVENT LISTENER TO OV SLIDE BTN
-ovPrevBtn.addEventListener('click', ovPrevSlide);
-ovNextBtn.addEventListener('click', ovNextSlide);
-
-
+ovPrevBtn.addEventListener("click", ovPrevSlide);
+ovNextBtn.addEventListener("click", ovNextSlide);
 
 // Add even listener to cart cont
-cartBtn.addEventListener('click', cartToggle);
-
+cartBtn.addEventListener("click", cartToggle);
 
 // Add event listener to order incress decrese count
-increseBtn.addEventListener('click', increseCount);
-decreseBtn.addEventListener('click', decreseCount);
+increseBtn.addEventListener("click", increseCount);
+decreseBtn.addEventListener("click", decreseCount);
 
 // Add event listener to add cart button
-addCartBtn.addEventListener('click', updateCart)
+addCartBtn.addEventListener("click", updateCart);
 
-// STEP: Add Event listener to profile 
-profile.addEventListener('click', profileToggle);
+// STEP: Add Event listener to profile
+profile.addEventListener("click", profileToggle);
 
 // STEP: add function with ovclosebtn
-ovCloseBtn.addEventListener('click', removeOverlay);
+ovCloseBtn.addEventListener("click", removeOverlay);
 
+// STEP: CHECKOUT BUTTON HANDLE
+checkBtn.addEventListener("click", showConfirmation);
+greetCloseBtn.addEventListener("click", hideConfirmation);
+
+// STEP: ADD EVENT LISTENER WITH NAV OPEN AND CLOSE BUTTON CLICK
+navOpenBtn.addEventListener("click", navOpenMenu);
+navCloseBtn.addEventListener("click", navCloseMenu);
 // STEP: SHOW OVERLAY GALLERY
 
 // NOTE: FUNCTION AREA
 
 // Toggle cart container function
-function cartToggle(){
+function cartToggle() {
   // console.log(cartBtn);
-  if(profCont.classList.contains('active')){
-    profCont.style.display = 'none';
-    profCont.classList.remove('active')
+  if (profCont.classList.contains("active")) {
+    profCont.style.display = "none";
+    profCont.classList.remove("active");
   }
-  if(!cartCont.classList.contains('active')) {
-    cartCont.style.display = 'block';
-    cartCont.classList.add('active');
+  if (!cartCont.classList.contains("active")) {
+    cartCont.style.display = "block";
+    cartCont.classList.add("active");
   } else {
-    cartCont.style.display = 'none';
-    cartCont.classList.remove('active')
+    cartCont.style.display = "none";
+    cartCont.classList.remove("active");
   }
 }
 
 // STEP: Toggle profile function
-function profileToggle(){
-  if(cartCont.classList.contains('active')){
-    cartCont.style.display = 'none';
-    cartCont.classList.remove('active');
+function profileToggle() {
+  if (cartCont.classList.contains("active")) {
+    cartCont.style.display = "none";
+    cartCont.classList.remove("active");
   }
-  if(!profCont.classList.contains('active')){
-    profCont.style.display = 'block';
-    profCont.classList.add('active');
-  } else{
-    profCont.style.display = 'none';
-    profCont.classList.remove('active');
+  if (!profCont.classList.contains("active")) {
+    profCont.style.display = "block";
+    profCont.classList.add("active");
+  } else {
+    profCont.style.display = "none";
+    profCont.classList.remove("active");
   }
 }
 
@@ -143,8 +156,8 @@ function prevSlide() {
 // Function for thumbnail navigation
 function displaySlide(index) {
   showSlide(index);
-  thums.forEach(thum => thum.classList.remove('active'));
-  thums[index].classList.add('active');
+  thums.forEach((thum) => thum.classList.remove("active"));
+  thums[index].classList.add("active");
 }
 
 function displayOverlaySlide(index, overlaySlides) {
@@ -154,16 +167,14 @@ function displayOverlaySlide(index, overlaySlides) {
   });
 }
 
-
-
 // Add click events to thumbnails
 thums.forEach((thum, index) => {
-  
-  thum.addEventListener('click', () => showOverlay(index, slides, nextSlide, prevSlide)); // Pass  // Assuming your showOverlay function
+  thum.addEventListener("click", () =>
+    showOverlay(index, slides, nextSlide, prevSlide)
+  ); // Pass  // Assuming your showOverlay function
 });
 
 showSlide(0);
-
 
 // ************************ STEP: OVERLAY ***************
 
@@ -188,8 +199,8 @@ function ovPrevSlide() {
 // Function for thumbnail navigation
 function displayOvSlide(index) {
   showOvSlide(index);
-  ovThums.forEach(ovThum => ovThum.classList.remove('active'));
-  ovThums[index].classList.add('active');
+  ovThums.forEach((ovThum) => ovThum.classList.remove("active"));
+  ovThums[index].classList.add("active");
 }
 
 function displayOverlaySlide(index, overlaySlides) {
@@ -199,11 +210,9 @@ function displayOverlaySlide(index, overlaySlides) {
   });
 }
 
-
-
 // Add click events to thumbnails
 ovThums.forEach((ovThum, index) => {
-  ovThum.addEventListener('click', () => displayOvSlide(index));
+  ovThum.addEventListener("click", () => displayOvSlide(index));
   // Pass  // Assuming your showOverlay function
 });
 
@@ -211,46 +220,43 @@ showOvSlide();
 
 // ************************ STEP: OVERLAY ***************
 
-
-
 // STEP: ORDER QUANTITY COUNT FUNCTIONALITIES ************
 
 let countValue = displayCount.value;
 let discountValue = 125;
 countValue = 1;
 // console.log("🚀 ~ countValue:", countValue)
-function increseCount(){
-countValue = countValue + 1;
-displayCount.textContent = countValue;
-discountValue += 125;
-discountPrice.textContent = `$${discountValue}`;
-// console.log("🚀 ~ increseCount ~ discountPrice:", discountPrice)
-}
-function decreseCount(){
-  if(displayCount.textContent > 1) {
-  countValue = countValue - 1;
-  displayCount.textContent = countValue - 1;
-} else{
-  console.warn('nagetive value not allowed');
-}
-if(discountValue > 125){
-  discountValue -= 125;
+function increseCount() {
+  countValue = countValue + 1;
+  displayCount.textContent = countValue;
+  discountValue += 125;
   discountPrice.textContent = `$${discountValue}`;
+  // console.log("🚀 ~ increseCount ~ discountPrice:", discountPrice)
 }
+function decreseCount() {
+  if (displayCount.textContent > 1) {
+    countValue = countValue - 1;
+    displayCount.textContent = countValue - 1;
+  } else {
+    console.warn("nagetive value not allowed");
+  }
+  if (discountValue > 125) {
+    discountValue -= 125;
+    discountPrice.textContent = `$${discountValue}`;
+  }
 }
-
 
 //STEP: update cart
-function updateCart(){
+function updateCart() {
   cartItemCount.textContent = countValue;
   cartItemPrice.textContent = `$${discountValue}`;
-  console.log("🚀 ~ updateCart ~ cartItemPrice:", cartItemPrice)
+  console.log("🚀 ~ updateCart ~ cartItemPrice:", cartItemPrice);
 }
 
-
 // STEP: remove overlay function
-function removeOverlay(){
-  ovGallery.classList.add('hide');
+function removeOverlay() {
+  console.log("working");
+  ovGallery.classList.add("hide");
 }
 
 // STEP: SHOW OVERLAY FUNCTION
@@ -260,8 +266,24 @@ function removeOverlay(){
 // }
 
 function showOverlay(index, slides, nextSlide, prevSlide) {
-  ovGallery.classList.remove('hide');
+  ovGallery.classList.remove("hide");
   currentOvSlide = index;
   displayOverlaySlide(index, ovSlides);
 }
 
+// STEP: CONFIRMATION BOX
+function showConfirmation() {
+  checkConfirm.classList.remove("hide");
+}
+
+function hideConfirmation() {
+  checkConfirm.classList.add("hide");
+}
+
+function navOpenMenu() {
+  mobileMenu.classList.add("mobile-menu-show");
+}
+
+function navCloseMenu() {
+  mobileMenu.classList.remove("mobile-menu-show");
+}
