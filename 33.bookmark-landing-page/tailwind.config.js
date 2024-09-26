9e2aec727550ab4df105436546e67688c0baf3d9
asadmash <mdasad.mash@gmail.com>
2024-09-26 10:21:16 +0600

    abcd

---
+++ 33.bookmark-landing-page/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [  "./**/*.html","./src/style.css"],
+  theme: {
+    extend: {
+      colors: {
+      'bg-soft-red': 'hsl(0, 94%, 66%)',
+      'grayish-blue': 'hsl(229, 8%, 60%)',
+      'light-grayish-blue': 'hsl(240, 24%, 96%)',
+      'dark-blue' : 'hsl(229, 31%, 21%)',
+      'soft-blue' : 'hsl(231, 69%, 60%)',
+      },
+    },
+  },
+  plugins: [],
+}
+
