8ebf9f8e9bf2ffeeae3f0228aea38e7f73d4cb64
asadmash <mdasad.mash@gmail.com>
2024-12-01 18:24:38 +0600

    tailwind.config content type updated

---
+++ 34.calculator-app-react/src/components/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <div className='bg-gray-800 text-white w-screen h-screen flex justify-center items-center'>
+        <div className='w-64 h-auto bg-gray-900 rounded-2xl shadow-xl border-4 border-gray-100'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Calculator
